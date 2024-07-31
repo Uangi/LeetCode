f2b17941770f89a089c45580825fc4cd2917a576
박진영 <97930866+Uangi@users.noreply.github.com>
2024-07-31 20:55:31 +0900

    Time: 12 ms (97.13%), Space: 46.8 MB (73.55%) - LeetHub

---
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.java
@@ -0,0 +1,23 @@
+import java.util.PriorityQueue;
+
+class KthLargest {
+    private PriorityQueue<Integer> heap;
+    private int k;
+
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        this.heap = new PriorityQueue<>();
+        
+        for (int num : nums) {
+            add(num);
+        }
+    }
+
+    public int add(int val) {
+        heap.offer(val);
+        if (heap.size() > k) {
+            heap.poll();
+        }
+        return heap.peek();
+    }
+}
