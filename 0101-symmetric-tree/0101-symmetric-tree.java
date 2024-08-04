ce7781d02bb648d528d80f21a22512ac1e005fd0
박진영 <97930866+Uangi@users.noreply.github.com>
2024-08-04 21:26:07 +0900

    Time: 0 ms (100%), Space: 41.6 MB (46.88%) - LeetHub

---
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true; // 빈 트리는 대칭
+        }
+        return isMirror(root.left, root.right);
+    }
+
+    private boolean isMirror(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null) {
+            return true; // 두 노드가 모두 null이면 대칭
+        }
+        if (t1 == null || t2 == null) {
+            return false; // 하나만 null이면 대칭이 아님
+        }
+        // 두 노드의 값이 같고, 각각의 서브트리가 서로 대칭인지 확인
+        return (t1.val == t2.val) && isMirror(t1.left, t2.right) && isMirror(t1.right, t2.left);
+    }
+}
